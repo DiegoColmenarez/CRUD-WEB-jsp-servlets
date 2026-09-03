ffdadfa54ea9299b701a4f3f2a546205116c3a6c
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 00:37:38 -0500

    chore/rename Pattern object

--- src/main/java/org/model/vo/UserName.java
+++ src/main/java/org/model/vo/UserName.java
@@ -7,11 +7,11 @@ import java.util.regex.Pattern;
 
 public record UserName(String value) {
 
-    private static final Pattern LAST_NAME_PATTERN =
+    private static final Pattern NAME_PATTERN =
             Pattern.compile("^[\\p{L} \\-']+$");
 
     private static void validateNameIsString(String value) {
-        if (!LAST_NAME_PATTERN.matcher(value).matches()) {
+        if (!NAME_PATTERN.matcher(value).matches()) {
             throw InvalidNameUserException.beacuseIsNotString();
         }
     }
