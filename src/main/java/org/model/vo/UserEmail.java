9b5758bdcab27fe1b4632786a878faad13d8a0f6
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 01:01:24 -0500

    feat/implementation of validateFormatEmail method in UserEmail record

--- src/main/java/org/model/vo/UserEmail.java
+++ src/main/java/org/model/vo/UserEmail.java
@@ -1,4 +1,18 @@
 package org.model.vo;
 
-public record UserEmail() {
+import org.model.exceptions.InvalidEmailUserException;
+
+import java.util.regex.Pattern;
+
+public record UserEmail(String value) {
+
+    // de momento esto se queda así
+    private static final Pattern PATTERN_EMAIL =
+            Pattern.compile("^[\\p{L} \\-']+$");
+
+    private static void validateFormatEmail(String value){
+        if (!PATTERN_EMAIL.matcher(value).matches()){
+            throw InvalidEmailUserException.becauseFormatIsInvalid();
+        }
+    }
 }
