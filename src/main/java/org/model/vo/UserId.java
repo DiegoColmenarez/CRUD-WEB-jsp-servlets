adc0f09a74162c962a74870d059c488677c70d83
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 01:08:38 -0500

    feat/add constructor of UserId

--- src/main/java/org/model/vo/UserId.java
+++ src/main/java/org/model/vo/UserId.java
@@ -2,11 +2,19 @@ package org.model.vo;
 
 import org.model.exceptions.InvalidUserIdException;
 
+import java.util.Objects;
+
 public record UserId(int value) {
 
     private static void validateIntPositive(int value){
         if (value < 0){
             throw InvalidUserIdException.becauseIdIsInvalid();
         }
+    }
+
+    public UserId{
+        var normalizedValue =  Objects.requireNonNull(value, "UserLastName cannot be null");
+        validateIntPositive(normalizedValue);
+        value = normalizedValue;
     }
 }
