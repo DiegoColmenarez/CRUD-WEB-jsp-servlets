321527fe3d96cbb310ad99c8f4e1c705071e2cab
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 01:36:41 -0500

    feat/implementation of isValidValue method in TypeUser enum

--- src/main/java/org/model/enums/TypeUser.java
+++ src/main/java/org/model/enums/TypeUser.java
@@ -3,4 +3,13 @@ package org.model.enums;
 public enum TypeUser {
     CLIENTE,
     ADMIN;
+
+    private static boolean isValidValue(final String value) {
+        for (final TypeUser role : values()) {
+            if (role.name().equalsIgnoreCase(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
