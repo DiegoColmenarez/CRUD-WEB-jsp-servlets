272ddbb081b030730b9ff9ab6e7ddfc230fdd821
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-02 23:59:20 -0500

    feat/implementation of becauseIdAlredyExist method in InvalidUserIdException class

---
+++ src/main/java/org/model/exceptions/InvalidUserIdException.java
@@ -0,0 +1,14 @@
+package org.model.exceptions;
+
+public class InvalidUserIdException extends DomainException{
+    public InvalidUserIdException(String message) {
+        super(message);
+    }
+
+    private static String MESSAGE_INVALID_ID = "The ID is invalid, because is a negative number";
+    private static String MESSAGE_EXIST_ID = "The ID is invalid, because alredy exist";
+
+    public static InvalidUserIdException becauseIdAlredyExist(){
+        return new InvalidUserIdException(MESSAGE_EXIST_ID);
+    }
+}
