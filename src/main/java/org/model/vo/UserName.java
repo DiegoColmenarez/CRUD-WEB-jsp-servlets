dac5f5b09df4c0b10c23d33093153c3af5a3775c
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 00:33:06 -0500

    feat/ add constructor of UserName

--- src/main/java/org/model/vo/UserName.java
+++ src/main/java/org/model/vo/UserName.java
@@ -2,9 +2,11 @@ package org.model.vo;
 
 import org.model.exceptions.InvalidNameUserException;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public record UserName(String value) {
+
     private static final Pattern LAST_NAME_PATTERN =
             Pattern.compile("^[\\p{L} \\-']+$");
 
@@ -12,5 +14,12 @@ public record UserName(String value) {
         if (!LAST_NAME_PATTERN.matcher(value).matches()) {
             throw InvalidNameUserException.beacuseIsNotString();
         }
+    }
+
+    public UserName{
+        final String normalizedValue = Objects.requireNonNull(value, "UserLastName cannot be null")
+                .trim();
+        validateNameIsString(normalizedValue);
+        value = normalizedValue;
     }
 }
