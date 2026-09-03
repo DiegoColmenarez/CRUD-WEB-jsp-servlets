94c89842e2f2f69289730e1d58f32143a60901ff
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 01:54:55 -0500

    feat/add constructor of UserType

--- src/main/java/org/model/vo/UserType.java
+++ src/main/java/org/model/vo/UserType.java
@@ -3,11 +3,20 @@ package org.model.vo;
 import org.model.enums.TypeUser;
 import org.model.exceptions.InvalidTypeUserException;
 
+import java.util.Objects;
+
 public record UserType(String value) {
 
     private static void validateType(String value){
         if (!TypeUser.isValidValue(value)){
             throw InvalidTypeUserException.becauseValueIsInvalid();
         }
+    }
+
+    public UserType{
+        final String normalizedValue = Objects.requireNonNull(value, "User type cannot be null")
+                .trim();
+        validateType(normalizedValue);
+        value = normalizedValue;
     }
 }
