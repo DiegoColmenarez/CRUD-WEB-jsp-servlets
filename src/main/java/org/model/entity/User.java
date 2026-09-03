f7ce4a0437d436db6d70ed2b376fd2d829f88d47
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 02:02:07 -0500

    feat/ add constructor of User

--- src/main/java/org/model/entity/User.java
+++ src/main/java/org/model/entity/User.java
@@ -1,4 +1,28 @@
 package org.model.entity;
 
+import org.model.vo.UserEmail;
+import org.model.vo.UserId;
+import org.model.vo.UserName;
+import org.model.vo.UserType;
+
 public class User {
+    private UserName name;
+    private UserName lastName;
+    private UserId id;
+    private UserEmail email;
+    private UserType type;
+
+    public User(
+            UserName name,
+            UserName lastName,
+            UserEmail email,
+            UserType type
+    ){
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.type = type;
+    }
+
+
 }
