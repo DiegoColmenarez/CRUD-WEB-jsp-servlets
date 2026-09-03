94735405498013d28e88c98ae66598dac18464d8
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 12:39:18 -0500

    feat/implementation of becauseIsEmpty method in InvalidNameUserException

--- src/main/java/org/model/exceptions/InvalidNameUserException.java
+++ src/main/java/org/model/exceptions/InvalidNameUserException.java
@@ -6,8 +6,13 @@ public class InvalidNameUserException extends DomainException{
     }
 
     private static String MESSAGE_INVALID = "The name is invalid";
+    private static String MESSAGE_INVALID_EMPTY = "The name is empty";
 
     public static InvalidNameUserException beacuseIsNotString(){
         return new InvalidNameUserException(MESSAGE_INVALID);
+    }
+
+    public InvalidNameUserException becauseIsEmpty(){
+        return new InvalidNameUserException(MESSAGE_INVALID_EMPTY);
     }
 }
