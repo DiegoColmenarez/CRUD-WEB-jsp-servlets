1416e6cf3608c545014fa3ed4175ff4b8bab5833
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 00:29:07 -0500

    feat/implement of validateNameIsString in UserName record

--- src/main/java/org/model/vo/UserName.java
+++ src/main/java/org/model/vo/UserName.java
@@ -1,4 +1,16 @@
 package org.model.vo;
 
-public record UserName() {
+import org.model.exceptions.InvalidNameUserException;
+
+import java.util.regex.Pattern;
+
+public record UserName(String value) {
+    private static final Pattern LAST_NAME_PATTERN =
+            Pattern.compile("^[\\p{L} \\-']+$");
+
+    private static void validateNameIsString(String value) {
+        if (!LAST_NAME_PATTERN.matcher(value).matches()) {
+            throw InvalidNameUserException.beacuseIsNotString();
+        }
+    }
 }
