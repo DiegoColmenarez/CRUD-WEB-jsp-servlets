d246556440f0d83c17c34887c1b1da07a88a25a2
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 01:02:36 -0500

    feat/add parameter constructor of UserEmail

--- src/main/java/org/model/vo/UserEmail.java
+++ src/main/java/org/model/vo/UserEmail.java
@@ -2,6 +2,7 @@ package org.model.vo;
 
 import org.model.exceptions.InvalidEmailUserException;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public record UserEmail(String value) {
@@ -14,5 +15,10 @@ public record UserEmail(String value) {
         if (!PATTERN_EMAIL.matcher(value).matches()){
             throw InvalidEmailUserException.becauseFormatIsInvalid();
         }
+    }
+
+    public UserEmail{
+        final String normalizedValue = Objects.requireNonNull(value, "UserLastName cannot be null");
+        validateFormatEmail(normalizedValue);
     }
 }
