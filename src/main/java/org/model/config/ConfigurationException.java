9ba57299f13ca3b01d20c60d0f4dece11139f0b0
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 23:54:29 -0500

    feat/ implementation of becauseNullArgument method

--- src/main/java/org/model/config/ConfigurationException.java
+++ src/main/java/org/model/config/ConfigurationException.java
@@ -8,7 +8,10 @@ public class ConfigurationException extends RuntimeException {
         super(message, cause);
     }
 
-    private final String MESSAGE_NULL_ARGUMENT = "The configuration failed, Configuration file not found.";
+    private static final String MESSAGE_NULL_ARGUMENT = "The configuration failed, Configuration file not found.";
     private final String MESSAGE_NO_PERMISSIONS = "The configuration failed, lack of operating system permissions.";
 
+    public static ConfigurationException becauseNullArgument(final Throwable cause){
+        return new ConfigurationException(MESSAGE_NULL_ARGUMENT, cause);
+    }
 }
