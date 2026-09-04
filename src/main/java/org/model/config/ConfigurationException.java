deeb73a0d988c734e6f3d17580e779dadd05ca28
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 23:50:15 -0500

    feat/add private constructor in CongfigurationException Class

--- src/main/java/org/model/config/ConfigurationException.java
+++ src/main/java/org/model/config/ConfigurationException.java
@@ -4,4 +4,11 @@ public class ConfigurationException extends RuntimeException {
     public ConfigurationException(String message) {
         super(message);
     }
+    private ConfigurationException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    private final String MESSAGE_NULL_ARGUMENT = "The configuration failed, Configuration file not found.";
+    private final String MESSAGE_NO_PERMISSIONS = "The configuration failed, lack of operating system permissions.";
+
 }
