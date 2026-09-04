4c6b7ce17f4fde037a262d625d7d62f986a5724b
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-04 17:43:42 -0500

    feat/implementation of becauseFormatIsInvalid method

--- src/main/java/org/model/exceptions/InvalidPasswordException.java
+++ src/main/java/org/model/exceptions/InvalidPasswordException.java
@@ -6,8 +6,15 @@ public class InvalidPasswordException extends DomainException{
     }
 
     private static final String MESSAGE_INVALID = "The password is invalid, it's empty";
+    private static final String MESSAGE_INVALID_FORMAT = "The password is invalid," +
+            " The password must contain at least: a special character, a number, a lowercase letter, " +
+            "and an uppercase letter. It cannot contain spaces.";
 
     public InvalidPasswordException becauseIsEmpty(){
         throw new InvalidPasswordException(MESSAGE_INVALID);
+    }
+
+    public InvalidPasswordException becauseFormatIsInvalid(){
+        throw new InvalidPasswordException(MESSAGE_INVALID_FORMAT);
     }
 }
