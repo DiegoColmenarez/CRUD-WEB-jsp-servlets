5033df47a7b8a985f4cd909c3ed1bc6dd8deb0fc
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-04 18:24:16 -0500

    feat/implementation of becauseEmailAlredy method

--- src/main/java/org/model/exceptions/InvalidEmailUserException.java
+++ src/main/java/org/model/exceptions/InvalidEmailUserException.java
@@ -7,12 +7,16 @@ public class InvalidEmailUserException extends DomainException {
 
     private static final String MESSAGE_INVALID = "Email is Invalid, it's not a Email";
     private static final String MESSAGE_INVALID_EMPTY = "Email is Invalid, It's empty.";
-
+    private static final String MESSAGE_EMAIL_EXIST = "Email is Invalid, This email is already registered.";
     public static InvalidEmailUserException becauseFormatIsInvalid(){
         return new InvalidEmailUserException(MESSAGE_INVALID);
     }
 
     public static InvalidEmailUserException becauseIsEmpty(){
         return new InvalidEmailUserException(MESSAGE_INVALID_EMPTY);
+    }
+
+    public static InvalidEmailUserException becauseEmailAlredy(){
+        return new InvalidEmailUserException(MESSAGE_EMAIL_EXIST);
     }
 }
