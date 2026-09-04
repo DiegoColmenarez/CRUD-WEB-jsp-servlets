7d50fd11acc1792da5eec8ec4b6bc185bc1d2ccb
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-03 23:59:46 -0500

    feat/implementation of bescauseNopermissions method

--- src/main/java/org/model/config/ConfigurationException.java
+++ src/main/java/org/model/config/ConfigurationException.java
@@ -9,9 +9,13 @@ public class ConfigurationException extends RuntimeException {
     }
 
     private static final String MESSAGE_NULL_ARGUMENT = "The configuration failed, Configuration file not found.";
-    private final String MESSAGE_NO_PERMISSIONS = "The configuration failed, lack of operating system permissions.";
+    private static final String MESSAGE_NO_PERMISSIONS = "The configuration failed, lack of operating system permissions.";
 
     public static ConfigurationException becauseNullArgument(final Throwable cause){
         return new ConfigurationException(MESSAGE_NULL_ARGUMENT, cause);
+    }
+
+    public static ConfigurationException becauseNoPermissions(final Throwable cause){
+        return new ConfigurationException(MESSAGE_NO_PERMISSIONS, cause);
     }
 }
