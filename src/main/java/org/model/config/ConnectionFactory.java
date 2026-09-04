cbdcee957b70f249f082085ab20c2020b4cc2399
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-04 00:30:13 -0500

    feat/add static configuration and URL loading to ConnectionFactory

--- src/main/java/org/model/config/ConnectionFactory.java
+++ src/main/java/org/model/config/ConnectionFactory.java
@@ -1,4 +1,14 @@
 package org.model.config;
 
+import java.util.Properties;
+
 public class ConnectionFactory {
+    private static final Properties DB_PROPERTIES;
+    private static final String DB_URL;
+
+    static {
+        DB_PROPERTIES = ConnectionConfig.loadDataBaseConfig();
+        DB_URL = DB_PROPERTIES.getProperty("DB_URL");
+    }
+
 }
