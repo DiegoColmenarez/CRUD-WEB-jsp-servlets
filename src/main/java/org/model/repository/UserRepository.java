84d66656f6a2c629a44b062be3257d5b7abecfd2
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-04 19:33:08 -0500

    feat/implementation of insertUser method

--- src/main/java/org/model/repository/UserRepository.java
+++ src/main/java/org/model/repository/UserRepository.java
@@ -2,6 +2,9 @@ package org.model.repository;
 
 import org.model.config.ConnectionFactory;
 import org.model.entity.User;
+import org.model.vo.UserEmail;
+import org.model.vo.UserName;
+import org.model.vo.UserPassword;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,15 +12,17 @@ import java.sql.SQLException;
 
 public class UserRepository {
 
-//    public void insertUser(User user){
-//        String sql = "INSERT INTO users (nombre, apellido, email, password) VALUES (?, ?, ?, ?)";
-//        try (Connection connection = ConnectionFactory.getConnection();
-//             PreparedStatement statement = connection.prepareStatement(sql)){
-//            statement.setString(1, String.valueOf(user.getName()));
-//            statement.setString(2,String.valueOf(user.getLastName()));
-//            statement.setString(3, String.valueOf(user.getEmail()));
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    public void insertUser(User user){
+        String sql = "INSERT INTO users (nombre, apellido, email, password) VALUES (?, ?, ?, ?)";
+        try (Connection connection = ConnectionFactory.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setString(1, String.valueOf(user.getName()));
+            statement.setString(2,String.valueOf(user.getLastName()));
+            statement.setString(3, String.valueOf(user.getEmail()));
+            statement.setString(4, String.valueOf(user.getPassword()));
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
