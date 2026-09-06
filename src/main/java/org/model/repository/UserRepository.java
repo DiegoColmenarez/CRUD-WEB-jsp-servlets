59089c8f3e86a8e52db331cd93af4832fe6421b6
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-06 14:04:33 -0500

    feat/deleteUser implementation method in UserRepository

--- src/main/java/org/model/repository/UserRepository.java
+++ src/main/java/org/model/repository/UserRepository.java
@@ -3,7 +3,10 @@ package org.model.repository;
 import org.model.config.ConnectionFactory;
 import org.model.entity.User;
 import org.model.exceptions.InvalidEmailUserException;
-import org.model.exceptions.InvalidNameUserException;
+import org.model.vo.UserEmail;
+import org.model.vo.UserId;
+import org.model.vo.UserName;
+import org.model.vo.UserPassword;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -24,6 +27,21 @@ public class UserRepository {
             if ("23505".equals(e.getSQLState())){
                 throw InvalidEmailUserException.becauseEmailAlredy();
             }
+            throw RepositoryException.repositoryGeneralException(e.getCause());
+        }
+    }
+
+    public void deleteUser(UserId id) {
+        String sql = "DELETE FROM users WHERE id = ?";
+        int rowsAffect = 0;
+        try (Connection connection = ConnectionFactory.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id.value());
+            rowsAffect = statement.executeUpdate();
+            if (rowsAffect == 0) {
+                throw new RuntimeException("No se afectaron filas");
+            }
+        } catch (SQLException e) {
             throw RepositoryException.repositoryGeneralException(e.getCause());
         }
     }
