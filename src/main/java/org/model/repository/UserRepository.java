ed08209cd51c5cdacb71e376ebc0f0a0008abc76
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-06 20:20:00 -0500

    feat/Implementation of UserNotFoundException class

--- src/main/java/org/model/repository/UserRepository.java
+++ src/main/java/org/model/repository/UserRepository.java
@@ -3,10 +3,7 @@ package org.model.repository;
 import org.model.config.ConnectionFactory;
 import org.model.entity.User;
 import org.model.exceptions.InvalidEmailUserException;
-import org.model.vo.UserEmail;
 import org.model.vo.UserId;
-import org.model.vo.UserName;
-import org.model.vo.UserPassword;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -14,23 +11,24 @@ import java.sql.SQLException;
 
 public class UserRepository {
 
-    public void insertUser(User user){
+    public void insertUser(User user) {
         String sql = "INSERT INTO users (nombre, apellido, email, password) VALUES (?, ?, ?, ?)";
         try (Connection connection = ConnectionFactory.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)){
+             PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, user.getName().value());
             statement.setString(2, user.getLastName().value());
             statement.setString(3, user.getEmail().value());
             statement.setString(4, user.getPassword().value());
             statement.executeUpdate();
         } catch (SQLException e) {
-            if ("23505".equals(e.getSQLState())){
+            if ("23505".equals(e.getSQLState())) {
                 throw InvalidEmailUserException.becauseEmailAlredy();
             }
             throw RepositoryException.repositoryGeneralException(e.getCause());
         }
     }
 
+    //esto está mal cambialo
     public void deleteUser(UserId id) {
         String sql = "DELETE FROM users WHERE id = ?";
         int rowsAffect = 0;
