d472ebad86ddf5f0dabc8c9b80da303e17bade7a
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-08 15:03:09 -0500

    fix/ catching the domain exception in showEditFormMethod

--- src/main/java/org/controller/UserServlet.java
+++ src/main/java/org/controller/UserServlet.java
@@ -146,16 +146,21 @@ public class UserServlet extends HttpServlet {
 
     private void showEditForm(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        RequestDispatcher dispatcher = null;
         try {
             int id = Integer.parseInt(request.getParameter("id"));
             UserId userId = new UserId(id);
             User user = userRepository.findById(userId);
             request.setAttribute("user", user);
-            RequestDispatcher dispatcher = request.getRequestDispatcher("jsp/user/modify.jsp");
+            dispatcher = request.getRequestDispatcher("jsp/user/modify.jsp");
             dispatcher.forward(request, response);
         } catch (NumberFormatException e) {
             response.sendRedirect("/menu.jsp");
-        }
+        } catch (DomainException e) {
+            request.setAttribute("errorMessage", e.getMessage());
+            dispatcher = request.getRequestDispatcher("jsp/user/searchModify.jsp");
+            dispatcher.forward(request, response);
+    }
     }
 
     private void updateUser(HttpServletRequest request, HttpServletResponse response)
