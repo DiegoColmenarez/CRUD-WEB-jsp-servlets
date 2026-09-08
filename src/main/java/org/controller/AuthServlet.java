a51df930c910888913616691893ec02dc7e81b60
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-07 22:04:53 -0500

    feat/showLoginForm method

--- src/main/java/org/controller/AuthServlet.java
+++ src/main/java/org/controller/AuthServlet.java
@@ -1,9 +1,14 @@
 package org.controller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.model.repository.UserRepository;
+
+import java.io.IOException;
 
 @WebServlet(name = "AuthServlet", value = "/auth")
 public class AuthServlet extends HttpServlet {
@@ -13,6 +18,11 @@ public class AuthServlet extends HttpServlet {
     public void init() throws ServletException {
         super.init();
         this.userRepository = new UserRepository();
+    }
+    private void showLoginForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("jsp/auth/login.jsp");
+        dispatcher.forward(request, response);
     }
 }
 
