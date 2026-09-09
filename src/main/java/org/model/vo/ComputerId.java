50b5b9efe9f3386902cdb15d3419c8a7bc495a47
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-09 15:45:34 -0500

    feat/ add validateNull method in ComputerId

--- src/main/java/org/model/vo/ComputerId.java
+++ src/main/java/org/model/vo/ComputerId.java
@@ -4,13 +4,20 @@ import org.model.exceptions.InvalidComputerIdException;
 
 public record ComputerId(Integer value) {
 
+    private static void validateNull(Integer value) {
+        if (value == null) {
+            throw InvalidComputerIdException.becauseIdIsNull();
+        }
+    }
+
     private static void validatePositive(Integer value) {
-        if (value != null && value <= 0) {
+        if (value <= 0) {
             throw InvalidComputerIdException.becauseIdIsInvalid();
         }
     }
 
     public ComputerId {
+        validateNull(value);
         validatePositive(value);
     }
 }
