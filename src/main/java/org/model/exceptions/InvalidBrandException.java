c241258fd096abcd0fa397aab7dc1262aab06c6d
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-09 15:47:28 -0500

    feat/ add becauseBrandIsNull, becauseBrandIsEmpty, becauseFormatIsInvalid methods in InvalidBrandException

--- src/main/java/org/model/exceptions/InvalidBrandException.java
+++ src/main/java/org/model/exceptions/InvalidBrandException.java
@@ -5,11 +5,21 @@ public class InvalidBrandException extends DomainException {
         super(message);
     }
 
-    private static final String MESSAGE_EMPTY = "Brand is invalid, it cannot be empty";
+    private static final String MESSAGE_BRAND_NULL = "Brand is invalid. Brand is Null";
+    private static final String MESSAGE_BRAND_EMPTY = "Brand is invalid, it cannot be empty";
+    private static final String MESSAGE_FORMAT_INVALID = "Brand format is invalid, only letters, numbers, spaces, hyphens and underscores are allowed";
     private static final String MESSAGE_LENGTH_INVALID = "Brand is invalid, length must be between 2 and 50 characters";
 
-    public static InvalidBrandException becauseIsEmpty() {
-        return new InvalidBrandException(MESSAGE_EMPTY);
+    public static InvalidBrandException becauseBrandIsNull() {
+        return new InvalidBrandException(MESSAGE_BRAND_NULL);
+    }
+
+    public static InvalidBrandException becauseBrandIsEmpty() {
+        return new InvalidBrandException(MESSAGE_BRAND_EMPTY);
+    }
+
+    public static InvalidBrandException becauseFormatIsInvalid() {
+        return new InvalidBrandException(MESSAGE_FORMAT_INVALID);
     }
 
     public static InvalidBrandException becauseLengthIsInvalid() {
