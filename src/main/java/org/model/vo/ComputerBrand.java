525c41883207b6bae34bd5d5fc050007ade594a0
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-09 15:49:44 -0500

    feat/ add validteFormat method in ComputerBrand

--- src/main/java/org/model/vo/ComputerBrand.java
+++ src/main/java/org/model/vo/ComputerBrand.java
@@ -1,26 +1,38 @@
 package org.model.vo;
 
 import org.model.exceptions.InvalidBrandException;
-import java.util.Objects;
 
 public record ComputerBrand(String value) {
 
-    private static void validateNotEmpty(String value) {
+    private static void validateNull(String value) {
+        if (value == null) {
+            throw InvalidBrandException.becauseBrandIsNull();
+        }
+    }
+
+    private static void validateEmpty(String value) {
         if (value.trim().isEmpty()) {
-            throw InvalidBrandException.becauseIsEmpty();
+            throw InvalidBrandException.becauseBrandIsEmpty();
         }
     }
 
     private static void validateFormat(String value) {
+        if (!value.matches("^[a-zA-Z0-9\\s\\-_]+$")) {
+            throw InvalidBrandException.becauseFormatIsInvalid();
+        }
+    }
+
+    private static void validateLength(String value) {
         if (value.length() < 2 || value.length() > 50) {
             throw InvalidBrandException.becauseLengthIsInvalid();
         }
     }
 
     public ComputerBrand {
-        final String normalizedValue = Objects.requireNonNull(value, "Brand cannot be null").trim();
-        validateNotEmpty(normalizedValue);
-        validateFormat(normalizedValue);
-        value = normalizedValue.toUpperCase();
+        validateNull(value);
+        validateEmpty(value);
+        validateFormat(value.trim());
+        validateLength(value.trim());
+        value = value.trim().toUpperCase();
     }
 }
