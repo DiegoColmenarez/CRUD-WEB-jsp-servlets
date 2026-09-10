eca55a5d64e524eaa92f81ace150afd11e57a5ae
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-09 23:49:40 -0500

    feat/ add showAddForm method in ComputerServlet

--- src/main/java/org/controller/ComputerServlet.java
+++ src/main/java/org/controller/ComputerServlet.java
@@ -1,9 +1,14 @@
 package org.controller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.model.repository.ComputerRepository;
+
+import java.io.IOException;
 
 @WebServlet(name = "ComputerServlet", value = "/computer")
 public class ComputerServlet extends HttpServlet {
@@ -13,5 +18,11 @@ public class ComputerServlet extends HttpServlet {
     public void init() throws ServletException {
         super.init();
         this.computerRepository = new ComputerRepository();
+    }
+
+    private void showAddForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/computer/add.jsp");
+        dispatcher.forward(request, response);
     }
 }
