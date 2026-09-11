6b936b950863cfcf9b06743f76ce14357738a0db
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-10 22:02:48 -0500

    feat/ add generator in PasswordResetCodeGenerator class

--- src/main/java/org/model/service/PasswordResetCodeGenerator.java
+++ src/main/java/org/model/service/PasswordResetCodeGenerator.java
@@ -1,4 +1,15 @@
 package org.model.service;
 
+import org.model.vo.PasswordResetCode;
+
+import java.security.SecureRandom;
+
 public class PasswordResetCodeGenerator {
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    public PasswordResetCode generator(){
+        int number = 100000 + secureRandom.nextInt(900000);
+        return new PasswordResetCode(String.valueOf(number));
+    }
 }
