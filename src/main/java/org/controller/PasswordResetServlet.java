5e28c6e48efa91fc1a62a398e12f7163672f63b9
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-11 00:13:48 -0500

    fet/ add showEmailForm method

--- src/main/java/org/controller/PasswordResetServlet.java
+++ src/main/java/org/controller/PasswordResetServlet.java
@@ -1,12 +1,17 @@
 package org.controller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.model.repository.PasswordResetRepository;
 import org.model.repository.UserRepository;
 import org.model.service.EmailService;
 import org.model.service.PasswordResetCodeGenerator;
+
+import java.io.IOException;
 
 @WebServlet(name = "PasswordResetServlet", value = "/password-reset")
 public class PasswordResetServlet extends HttpServlet {
@@ -23,5 +28,11 @@ public class PasswordResetServlet extends HttpServlet {
         this.passwordResetRepository = new PasswordResetRepository();
         this.codeGenerator = new PasswordResetCodeGenerator();
         this.emailService = new EmailService();
+    }
+
+    private void showEmailForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/password-reset/enterEmail.jsp");
+        dispatcher.forward(request, response);
     }
 }
