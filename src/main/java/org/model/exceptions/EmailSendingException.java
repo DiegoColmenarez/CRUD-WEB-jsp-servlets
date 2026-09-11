a3386212a5d527c833c2b63f8ba6725b544847fd
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-10 21:44:45 -0500

    feat/ add becauseSendFailed factory method

--- src/main/java/org/model/exceptions/EmailSendingException.java
+++ src/main/java/org/model/exceptions/EmailSendingException.java
@@ -4,4 +4,10 @@ public class EmailSendingException extends DomainException {
     public EmailSendingException(String message) {
         super(message);
     }
+
+    private static final String MESSAGE_SEND_FAILED = "Email could not be sent, please try again later";
+
+    public static EmailSendingException becauseSendFailed() {
+        return new EmailSendingException(MESSAGE_SEND_FAILED);
+    }
 }
