b51e2b7f9fa4b7d7505cac73faef15c5e4451f82
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-10 22:52:15 -0500

    refactor/ cache the Session as static

--- src/main/java/org/model/service/EmailService.java
+++ src/main/java/org/model/service/EmailService.java
@@ -8,35 +8,33 @@ import org.model.exceptions.EmailSendingException;
 
 import java.util.Properties;
 
-
 public class EmailService {
 
-    private final String smtpUser;
+    private static final Session SESSION;
+    private static final String SMTP_USER;
 
-    public EmailService() {
-        this.smtpUser = PropertiesLoaderSmtp.get("smtp.user");
-    }
-
-    public void sendResetCode(String toEmail, String code) {
+    static {
+        SMTP_USER = PropertiesLoaderSmtp.get("smtp.user");
         Properties props = new Properties();
         props.put("mail.smtp.auth", PropertiesLoaderSmtp.get("smtp.auth"));
         props.put("mail.smtp.starttls.enable", PropertiesLoaderSmtp.get("smtp.starttls"));
         props.put("mail.smtp.host", PropertiesLoaderSmtp.get("smtp.host"));
         props.put("mail.smtp.port", PropertiesLoaderSmtp.get("smtp.port"));
-
-        Session session = Session.getInstance(props, new Authenticator() {
+        SESSION = Session.getInstance(props, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(
-                        PropertiesLoaderSmtp.get("smtp.user"),
+                        SMTP_USER,
                         PropertiesLoaderSmtp.get("smtp.password")
                 );
             }
         });
+    }
 
+    public void sendResetCode(String toEmail, String code) {
         try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(smtpUser));
+            Message message = new MimeMessage(SESSION);
+            message.setFrom(new InternetAddress(SMTP_USER));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
             message.setSubject("Password Reset Code");
             message.setText("Your password reset code is: " + code +
