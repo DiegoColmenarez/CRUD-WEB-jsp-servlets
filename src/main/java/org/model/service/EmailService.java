d26d3513e68e0835b070d92718d4aa59a48418eb
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-10 22:34:17 -0500

    feat/ add EmailService Constructor

--- src/main/java/org/model/service/EmailService.java
+++ src/main/java/org/model/service/EmailService.java
@@ -1,4 +1,15 @@
 package org.model.service;
 
+import jakarta.mail.*;
+import org.model.config.PropertiesLoaderSmtp;
+
+
 public class EmailService {
+
+    private final String smtpUser;
+
+    public EmailService() {
+        this.smtpUser = PropertiesLoaderSmtp.get("smtp.user");
+    }
+
 }
