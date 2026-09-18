ece963dc40d5861c4402f1fcef895458e7e10056
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-18 04:30:30 -0500

    feat/add variables the entorn

--- src/main/java/org/model/service/EmailService.java
+++ src/main/java/org/model/service/EmailService.java
@@ -12,17 +12,22 @@ public class EmailService {
 
     private static final Session SESSION;
     private static final String SMTP_USER;
+    private static final String SMTP_FROM;
 
     static {
-
         String envUser = System.getenv("SMTP_USER");
         String envPassword = System.getenv("SMTP_PASSWORD");
         String envHost = System.getenv("SMTP_HOST");
         String envPort = System.getenv("SMTP_PORT");
+        String envFrom = System.getenv("SMTP_FROM");
+
         SMTP_USER = (envUser != null && !envUser.isEmpty()) ? envUser : PropertiesLoaderSmtp.get("smtp.user");
+        SMTP_FROM = (envFrom != null && !envFrom.isEmpty()) ? envFrom : PropertiesLoaderSmtp.get("smtp.from");
+
         String smtpPassword = (envPassword != null && !envPassword.isEmpty()) ? envPassword : PropertiesLoaderSmtp.get("smtp.password");
         String smtpHost = (envHost != null && !envHost.isEmpty()) ? envHost : PropertiesLoaderSmtp.get("smtp.host");
         String smtpPort = (envPort != null && !envPort.isEmpty()) ? envPort : PropertiesLoaderSmtp.get("smtp.port");
+
         String auth = PropertiesLoaderSmtp.get("smtp.auth") != null ? PropertiesLoaderSmtp.get("smtp.auth") : "true";
         String startTls = PropertiesLoaderSmtp.get("smtp.starttls") != null ? PropertiesLoaderSmtp.get("smtp.starttls") : "true";
 
@@ -43,7 +48,7 @@ public class EmailService {
     public void sendResetCode(String toEmail, String code) {
         try {
             Message message = new MimeMessage(SESSION);
-            message.setFrom(new InternetAddress(SMTP_USER));
+            message.setFrom(new InternetAddress(SMTP_FROM));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
             message.setSubject("Password Reset Code");
             message.setText("Your password reset code is: " + code +
